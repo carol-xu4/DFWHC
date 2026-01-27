b46303e2cf56cfba02b07f21128d535b765655ec
carol-xu4 <xucarol.20@gmail.com>
2026-01-26 22:28:14 -0500

    Initial Commit: DFWHC analysis

---
+++ data-code/cleaning.r
@@ -0,0 +1,6 @@
+## Preliminaries -----------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata, readr)
+
+## Set working directory
+setwd("C:/Users/xucar/Desktop/mortality")
