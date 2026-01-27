8d0a40587cb65e8d21ae144ec7dd8f49b6881fae
carol-xu4 <xucarol.20@gmail.com>
2026-01-26 22:34:39 -0500

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# DFW Hospital Council Data (2023-2025) 
+## DFWHC Outpatient Analysis - Report Builder
+### 01/26/2026 data pull: 
+Prn_Diag_Desc	D_Mnth_Yr	D_Year	Hosp_Name_Short	Hosp_County	Stat_Payer_Primary	Stat_Payer_Group	Pt_Race	Pt_Gender	Pt_Ethnicity	Pt_AgeGroup
--- data-code/cleaning.r
+++ data-code/cleaning.r
@@ -3,4 +3,22 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata, readr)
 
 ## Set working directory
-setwd("C:/Users/xucar/Desktop/mortality")
+setwd("C:/Users/xucar/Desktop/DFWHC")
+
+## Read in data ------------------------------------------------------------
+dfwhc1 = read_excel("data/input/DFWHC1-26.xlsx")
+
+dfwhc1 = dfwhc1 %>%
+    rename(
+        dx = Prn_Diag_Desc,
+        month_year = D_Mnth_Yr,
+        year = D_Year,
+        hospital = Hosp_Name_Short,
+        hosp_county = Hosp_County,
+        payer = Stat_Payer_Primary,
+        payer_group = Stat_Payer_Group,
+        race = Pt_Race,
+        gender = Pt_Gender,
+        ethnicity = Pt_Ethnicity,
+        age_group = Pt_AgeGroup
+    )
