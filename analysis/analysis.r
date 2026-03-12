dd35be226bada7891e75a21e398fccc97047ca17
carol-xu4 <xucarol.20@gmail.com>
2026-03-11 20:35:02 -0400

    jps updates

--- analysis/analysis.r
+++ analysis/analysis.r
@@ -239,8 +239,13 @@ write_csv(hospital_counts, "results/hospital_counts.csv")
 # JPS Hospital substance counts
 jps_substances = dfwhc1 %>%
     filter(hospital == "JPS Hosp") %>%
-    count(substance, sort = TRUE)
+    count(substance, sort = TRUE) 
 write_csv(jps_substances, "results/jps_substances")
+
+jps_substances_year = dfwhc1 %>%
+    filter(hospital == "JPS Hosp") %>%
+    count(substance, year, sort = TRUE) 
+write_csv(jps_substances_year, "results/jps_substances_year")
 
 jps_monthly = dfwhc1 %>%
     mutate(month = as.Date(paste0(month_year, "01"), format = "%Y%m%d")) %>%
